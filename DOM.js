fbe71d94ae13c92dbc4521959b23d3e60133c2e3
Anshikagupta21 <anshikagupta663@gamil.com>
2025-04-10 18:18:30 +0530

    updated

--- DOM.js
+++ DOM.js
@@ -14,8 +14,169 @@ function okk(){
     let show= document.querySelector("#text")
     show.innerHTML="anshika"  // for html in javascript
     show.style.color="red"  ///for css in javascript
-    show.style.backgroundcolor="blue"
+    show.style.backgroundColor="blue"
 }
+
+//     // document.getElementById('text')
+//     // document.getElementsByClassName('text')
+//     // document.querySelector("#text")
+
+//   function okk(){
+    
+//   let show=  document.querySelector("#text")
+
+//   show.innerHTML="Siddharth"
+//   show.style.color="red"
+
+// //    document.querySelector("#text").innerHTML="Siddharth"
+
+
+//   }
+
+
+
+   // ===========  Normal function ================ 
+
+
+  //  function name(){
+  //     console.log("Normal Fun");
+          
+  //  }
+
+  //  name();
+
+
+  //  //  ========== Anonymus Function  ========
+
+
+  // let okk= function(){
+
+  //    console.log("Anonymus Function");
+     
+  //  }
+
+  //  okk();
+
+
+  //  // ========= IIFE==========
+
+  //  (
+  //   function(){
+  //     console.log("IIFE");
+      
+  //   }
+  //  )()
+
+
+  //  // =============  Arrow Function ("=>")   ========
+
+
+  //  let sid = ()=>{
+
+  //   console.log("Arrow Function");    
+
+  //  }
+
+  //  sid()
+
+
+
+// document.getElementById("name")
+// document.getElementsByClassName("name")
+// document.querySelector("#name")
+
+ //let orange= ()=>{
+  
+     //let BOX1= document.querySelector("#box")
+   
+      //BOX1.src="./img2.jpeg"
+     
+     //BOX1.style.backgroundImage='url(./img2.jpeg)'     
+   
+//}
+   
+//       let Green=()=>{
+   
+//        let BOX1= document.querySelector("#box")
+//         //BOX1.style.backgroundImage="none"    
+//          BOX1.style.backgroundColor="green"
+   
+//      }
+
+
+//by arrow function
+
+     let Orange=()=> {
+    let BOX1 = document.querySelector("#box")
+         BOX1.style.backgroundColor="orange"
+    
+ 
+ }
+  
+   let Green=()=>{
+   
+    let BOX1= document.querySelector("#box")
+     //BOX1.style.backgroundImage="none"    
+      BOX1.style.backgroundColor="green"
+
+  }
+
+
+let white= ()=>{
+    let BOX1= document.querySelector("#box")
+            BOX1.style.backgroundColor="grey"
+
+
+ }
+
+ let box1= ()=>{
+    let show= document.querySelector(".container")
+        //  show.style.backgroundColor="yellow"
+        show.style.backgroundImage="url(./Bugatti-Divo.webp)"
+ }
+
+ let box2= ()=>{
+    let show= document.querySelector(".container")
+           // show.style.backgroundColor="pink"
+            show.style.backgroundImage="url(./Bugatti-Divo.webp)"
+
+
+ }
+
+ let box3= ()=>{
+    let show= document.querySelector(".container")
+            show.style.backgroundColor="aqua"
+
+
+ }
+
+let pixel1=()=>{
+   let show= document.querySelector("#anshika")
+   show.style.fontSize="30px"
+}
+
+let pixel2=()=>{
+   let show= document.querySelector("#anshika")
+   show.style.fontSize="50px"
+}
+
+let pixel3=()=>{
+   let show= document.querySelector("#anshika")
+   show.style.fontSize="70px"
+}
+
+
+let sss=()=>{
+   let show= document.querySelector("#anshika")
+   show.style.display="none"
+}
+
+let hhh=()=>{
+   let show= document.querySelector("#anshika")
+   show.style.display="block"
+}
+
+
 //short cut
 //document.querySelector("#text").show.innerHTML="anshika"
 
